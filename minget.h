cc317c1ad9ef0add3482b719a5a07cc2d6194088
abigailibarrola <aibarrol@calpoly.edu>
2019-06-07 09:46:44 -0700

    merged and fixed typedef stuff

--- minget.h
+++ minget.h
@@ -66,7 +66,7 @@
 #include <time.h>
 
 /* Struct definition for a partition table entry */
-struct __attribute__ ((__packed__)) pt_entry {
+typedef struct __attribute__ ((__packed__)) pt_entry {
 	uint8_t bootind; 			/* Boot magic number*/
 	uint8_t start_head; 		/* Start of partition in CHS */
 	uint8_t start_sec_cyl[2]; 	/* See note on sec_cyl addressing */
@@ -75,7 +75,7 @@ struct __attribute__ ((__packed__)) pt_entry {
 	uint8_t end_sec_cyl[2]; 	/* See note on sec_cyl addressing */
 	uint32_t lFirst; 			/* First sector*/
 	uint32_t size; 				/* size of partition*/
-};
+} pt_entry;
 
 /* Struct definition for the superblock */
 struct __attribute__ ((__packed__)) superblock {
